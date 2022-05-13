a3e61c29e4b9768449587b4d865d980997509aa3
A N U J  M U T H A <mutha.am16@gmail.com>
2022-05-13 13:04:33 +0530

    Update hello_word.py

--- Assignment CC -2 Installation/GAE/hello_word.py
+++ Assignment CC -2 Installation/GAE/hello_word.py
@@ -4,4 +4,4 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.write("Hello World..!")
 
-app = webapp2.WSGIApplication([('/',MainPage)], debug=true)
+app = webapp2.WSGIApplication([('/',MainPage)], debug=True)
